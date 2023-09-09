2a13060bcab4351f74915453607f40b5b428d1cc
TheSaviourEking <saviour.eking@outlook.com>
2023-09-09 03:10:04 +0100

    add solution

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 // Your code here
+const Employee = require('./employee');
+
+const employee  = new Employee('John Wick', 'Dog Lover');
+
+setTimeout(employee.sayName.bind(employee), 2);
+
+setTimeout(employee.sayOccupation.bind(employee), 3000)
